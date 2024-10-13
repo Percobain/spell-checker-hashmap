4e8e1cf782db2fbfb7a1a6f500a60c13fcb75e4d
SM006 <shreyam1005@gmail.com>
2024-10-13 15:19:16 +0530

    added remaining functions to hashMap

--- hashMap.hpp
+++ hashMap.hpp
@@ -143,4 +143,129 @@ class HashMap
 		return false;
 	}
 
+	/*
+	 * Attemps to locate the key-value pair specified by the key parameter from the hash table.
+	 * Returns true if the key-value pair is found in the table and false otherwise*/
+	bool mapContains(const K &key) 
+	{
+		int index = HASH_FUNCTION(key) % mapCapacity();
+		if (index < 0)
+			index += mCapacity;
+
+		HashLink<K, V> *temp = mTable[index];
+		while (temp != nullptr) {
+			if (temp->getKey().compare(key) == 0)
+				return true;
+			temp = temp->getNext();
+		}
+
+		return false;
+	}
+
+	/*Returns the number of links in the hash table*/
+	int mapSize() const { 
+		return mSize; 
+		}
+
+	/* Returns number of buckets in the hash table */
+	int mapCapacity() const { 
+		return mCapacity; 
+		} 
+
+	/* Returns a pointer to the bucket in the table specified by the index */
+	HashLink<K, V>* mapTableLink(int index) const { return mTable[index]; }
+
+	/* Returns the number of table buckets without any links */
+	int mapEmptyBuckets() const {
+		int empty = 0;
+		for (int i = 0; i < mapCapacity(); i++) {
+			if (mTable[i] == nullptr)
+				empty++;
+		}
+
+		return empty;
+	}
+
+
+	/*
+	 Resizes the hash table to contain newCapacity number of buckets. After the new
+	 table is allocated, all old links are re-hashed into the new table. The old table
+	 memory is then deallocated, including all of the old links*/
+
+
+	void resizeTable(int newCapacity) {
+
+		// keep reference to old table for copy and delete
+		HashLink<K, V> **oldTable = mTable;
+		int oldCapacity = mCapacity;
+		mCapacity = newCapacity;
+
+		// allocate new hash map with updated capacity
+		mTable = new HashLink<K, V> *[newCapacity]();
+
+		for (int i = 0; i < newCapacity; i++)		
+			mTable[i] = nullptr;
+
+		// reset size counter and update new size with mapPut()
+		mSize = 0;
+		for (int i = 0; i < oldCapacity; i++) {
+			HashLink<K, V> *temp = oldTable[i];
+
+			// rehash links from old table into new table
+			while (temp != nullptr) {
+				K tempKey = temp->getKey();
+				V tempVal = temp->getValue();
+				mapPut(tempKey, tempVal);
+				temp = temp->getNext();
+			}
+		}
+
+		// delete links from old table, set new table capacity, and free the old table
+		hashMapCleanup(oldTable, oldCapacity);
+		delete[] oldTable;
+	}
+
+	/*
+	 * Overload operator << for HashMap print functionality
+	 * Prints the buckets and links in the map in the format Bucket n -> (key, value) */
+	friend std::ostream& operator<<(std::ostream& os, const HashMap<K, V>& map) {
+		for (int i = 0; i < map.mapCapacity(); i++) {
+			HashLink<K, V> *entry = map.mTable[i];
+			if (entry != nullptr) {
+				os << "Bucket " << i << " -> ";
+				while (entry != nullptr) {
+					os << "(" << entry->getKey() << ", " << entry->getValue() << ") -> ";
+					entry = entry->getNext();
+				}
+				os << "nullptr";
+				os << endl;
+			}
+		}
+		os << endl;
+		return os;
+	}
+
+	/* Hashes the key by folding (summing) each character */
+	int hashFunction1(const K &key) {
+		int r = 0;
+		for (int i = 0; key[i] != '\0'; i++)
+			r += key[i];
+
+		return r;
+	}
+
+	/* Hashes the key by shifting the value of each character, then folding (summing)
+	 hashFunction2 prevents anagrams hashing to the same value (via the shift), thus resulting in fewer collisions */
+	int hashFunction2(const K &key) {
+		int r = 0;
+		for (int i = 0; key[i] != '\0'; i++)
+			r += (i + 1) * key[i];
+
+		return r;
+	}
+
+	private:
+	HashLink<K, V>** mTable;
+	int mSize; 
+	int mCapacity; 
 };
