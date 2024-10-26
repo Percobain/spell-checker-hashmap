164a5d84863ef151188c0645bf53c9732cdbbc4a
Shreyans T <shreyans.tatiya@gmail.com>
2024-10-26 14:43:21 +0530

    revamp

--- hashMap.hpp
+++ hashMap.hpp
@@ -11,10 +11,6 @@ using namespace std;
 template <typename K, typename V>
 class HashMap {
 public:
-	/*
-	 * Parameterized HashMap constructor
-	 * @param capacity
-	 */
 	HashMap(int capacity) {
 		mCapacity = capacity;
 		mSize = 0;
@@ -23,14 +19,9 @@ public:
 			mTable[i] = nullptr;
 	}
 
-	/*
-	 * HashMap destructor
-	 * Frees link pointers in buckets and delete table
-	 */
 	~HashMap() { 
 		HashLink<K, V> *temp, *next;
 
-		// free all links in each bucket
 		for (int i = 0; i < mCapacity; i++) {
 			temp = mTable[i];
 			while (temp != nullptr) {
@@ -42,14 +33,7 @@ public:
 		delete[] mTable;
 	}
 
-	/*
-	 * Returns a pointer to the value of the link with the given key. Returns nullptr if no 
-	 * link with the given key is in the table.
-	 * @param key 
-	 * @return link value or nullptr 
-	 */
 	V* mapGet(const K &key) {
-		// get index of bucket
 		int index = HASH_FUNCTION(key) % mapCapacity();
 		if (index < 0)
 			index += mapCapacity();
@@ -66,30 +50,19 @@ public:
 		return nullptr;
 	}
 
-	/*
-	 * Updates the given key-value pair in the hash table if a link with the given key
-	 * already exists. Otherwise allocates a new link with the given key-value pair and adds it
-	 * to the corresponding bucket's linked list.
-	 * @param key 
-	 * @param value 
-	 */
 	void mapPut(const K key, const V value) {
-		// resize table if table load exceeds max threshold (default .75)
 		if (mapTableLoad() >= MAX_TABLE_LOAD)
 			resizeTable(mCapacity * 2);
 
-		// get index of bucket
 		int index = HASH_FUNCTION(key) % mapCapacity();
 		if (index < 0)
 			index += mCapacity;
 
-		// update link value if key exists in table
 		if (mapContains(key)) {
 			HashLink<K, V> *entry = mTable[index];
 			entry->setValue(value);
 		}
 
-		// otherwise add new link to bucket
 		else {
 			HashLink<K, V> *newLink = new HashLink<K, V> (key, value);
 
@@ -97,7 +70,6 @@ public:
 				mTable[index] = newLink;
 
 			else {
-				// add new link to end of bucket
 				HashLink<K, V> *temp = mTable[index];
 				while (temp->getNext() != nullptr) 
 					temp = temp->getNext();
@@ -108,12 +80,6 @@ public:
 		}
 	}
 
-	/*
-	 * Attemps to remove the key-value pair specified by the key parameter from the hash table.
-	 * Returns true if removal was successful and false otherwise
-	 * @param key 
-	 * @return bool indicating whether key-value pair removal was successful
-	 */
 	bool mapRemove(const K &key) {
 		if (mapContains(key)) {
 			int index = HASH_FUNCTION(key) % mapCapacity();
@@ -143,12 +109,6 @@ public:
 		return false;
 	}
 
-	/*
-	 * Attemps to locate the key-value pair specified by the key parameter from the hash table.
-	 * Returns true if the key-value pair is found in the table and false otherwise
-	 * @param key 
-	 * @return bool indicating whether key exists in table
-	 */
 	bool mapContains(const K &key) {
 		int index = HASH_FUNCTION(key) % mapCapacity();
 		if (index < 0)
@@ -165,28 +125,12 @@ public:
 		return false;
 	}
 
-	/*
-	 * Returns the number of links in the hash table
-	 * @return number of links
-	 */
 	int mapSize() const { return mSize; }
 
-	/*
-	 * Returns number of buckets in the hash table
-	 * @return number of buckets
-	 */
 	int mapCapacity() const { return mCapacity; } 
 
-	/*
-	 * Returns a pointer to the bucket in the table specified by the index
-	 * @returns HashLink pointer to specified bucket index
-	 */
 	HashLink<K, V>* mapTableLink(int index) const { return mTable[index]; }
 
-	/*
-	 * Returns the number of table buckets without any links
-	 * @return numbeer of empty buckets
-	 */
 	int mapEmptyBuckets() const {
 		int empty = 0;
 		for (int i = 0; i < mapCapacity(); i++) {
@@ -197,26 +141,15 @@ public:
 		return empty;
 	}
 
-	/*
-	 * Returns the ratio of (links / buckets) in the table currently
-	 * @return map table load
-	 */
 	double mapTableLoad() const {
 		double links = (double)mapSize();
 		double buckets = (double)mapCapacity();
 		return (links / buckets);
 	}
 
-	/*
-	 * Removes all links in the table and frees allocated memory. Used as a helper function
-	 * in the resizeTable() method when the table load exceeds MAP_TABLE_LOAD threshold
-	 * @param hash table to be cleaned
-	 * @param capacity of table
-	 */
 	void hashMapCleanup(HashLink<K, V> **map, int capacity) {
 		HashLink<K, V> *temp, *next;
 
-		// free link pointers in each bucket
 		for (int i = 0; i < capacity; i++) {
 			temp = map[i];
 			while (temp != nullptr) {
@@ -227,30 +160,20 @@ public:
 		}
 	}
 
-	/*
-	 * Resizes the hash table to contain newCapacity number of buckets. After the new
-	 * table is allocated, all old links are re-hashed into the new table. The old table
-	 * memory is then deallocated, including all of the old links
-	 * @param new capacity (number of buckets)
-	 */
 	void resizeTable(int newCapacity) {
-		// keep reference to old table for copy and delete
 		HashLink<K, V> **oldTable = mTable;
 		int oldCapacity = mCapacity;
 		mCapacity = newCapacity;
 
-		// allocate new hash map with updated capacity
 		mTable = new HashLink<K, V> *[newCapacity]();
 
 		for (int i = 0; i < newCapacity; i++)		
 			mTable[i] = nullptr;
 
-		// reset size counter and update new size with mapPut()
 		mSize = 0;
 		for (int i = 0; i < oldCapacity; i++) {
 			HashLink<K, V> *temp = oldTable[i];
 
-			// rehash links from old table into new table
 			while (temp != nullptr) {
 				K tempKey = temp->getKey();
 				V tempVal = temp->getValue();
@@ -259,18 +182,11 @@ public:
 			}
 		}
 
-		// delete links from old table, set new table capacity, and free the old table
 		hashMapCleanup(oldTable, oldCapacity);
 		delete[] oldTable;
 	}
 
 
-	/*
-	 * Overload operator << for HashMap print functionality
-	 * Prints the buckets and links in the map in the format Bucket n -> (key, value)
-	 * @param output stream os, HashMap map
-	 * @returns output stream containing formatted HashMap contents
-	 */
 	friend std::ostream& operator<<(std::ostream& os, const HashMap<K, V>& map) {
 		for (int i = 0; i < map.mapCapacity(); i++) {
 			HashLink<K, V> *entry = map.mTable[i];
@@ -288,10 +204,6 @@ public:
 		return os;
 	}
 
-	/*
-	 * Hashes the key by folding (summing) each character
-	 * @returns hashed value for input key
-	 */
 	int hashFunction1(const K &key) {
 		int r = 0;
 		for (int i = 0; key[i] != '\0'; i++)
@@ -300,12 +212,6 @@ public:
 		return r;
 	}
 
-	/*
-	 * Hashes the key by shifting the value of each character, then folding (summing)
-	 * hashFunction2 prevents anagrams hashing to the same value (via the shift), thus
-	 * resulting in fewer collisions
-	 * @returns hashed value for input key
-	 */
 	int hashFunction2(const K &key) {
 		int r = 0;
 		for (int i = 0; key[i] != '\0'; i++)
@@ -316,6 +222,6 @@ public:
 
 private:
 	HashLink<K, V>** mTable;
-	int mSize; // number of links in the table
-	int mCapacity; // number of buckets
+	int mSize;
+	int mCapacity;
 };
--- spellchecker.cpp
+++ spellchecker.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-// prototypes
 int loadDictionary(string fname, HashMap<string, int> *map);
 int calcLD(string word1, string word2);
 void spellChecker(HashMap<string, int> *dictionary);
@@ -15,7 +14,7 @@ int main() {
 	HashMap<string, int> *dictionary = new HashMap<string, int> (1000);
 
 	cout << "Loading dictionary file..." << '\n';
-	// load dictionary into hash map
+
 	start = clock();
 	int loadStatus = loadDictionary(dictionaryFile, dictionary);
 	end = clock();
@@ -32,20 +31,12 @@ int main() {
 	cout << "Dictionary contains " << dictionary->mapSize() << " entries hashed into " << dictionary->mapCapacity() << " buckets." << '\n';
 	cout << "Table load: " << dictionary->mapTableLoad() << '\n';
 
-	// run spellChecker with loaded dictionary
 	spellChecker(dictionary);
 
 	delete dictionary;
 	return 0;
 }
 
-/********** function implementation **********/
-/*	 
-* Loads the dictionary.txt file into the hash table
-* Returns 0 on success and -1 otherwise
-* @param dictionary file name (dictionary.txt) and ptr to hash map
-* @return int indicating whether load was successful
-*/
 int loadDictionary(string fname, HashMap<string, int> *map) {
 	string inputBuffer = "";
 	ifstream dictionaryFile(fname);
@@ -72,22 +63,17 @@ int calcLD(string word1, string word2) {
 	if (word2Len == 0)
 		return word1Len;
 
-	// construct matrix containing 0...word1Len + 1 rows and 0...word2Len + 1 columns
 	int matrix[word1Len + 1][word2Len + 1];
 
-	// initialize first column to 0...word1Len
 	for (int i = 0; i <= word1Len; i++)
 		matrix[i][0] = i;
 
-	// initialize first row to 0...word2Len
 	for (int i = 0; i <= word2Len; i++)
 		matrix[0][i] = i;
 
-	// examine each character of word1
 	for (int i = 1; i <= word1Len; i++) {
 		char c1 = word1[i - 1];
 
-		// examine each character of word2
 		for (int j = 1; j <= word2Len; j++) {
 			char c2 = word2[j - 1];
 
@@ -125,25 +111,16 @@ void spellChecker(HashMap<string, int> *dictionary) {
 		if (inputBuffer.compare("quit") == 0)
 			quit = true;
 
-		// spell check logic
-		// input word is spelled correctly if word is found in hash table
 		else if (dictionary->mapContains(inputBuffer))
 			cout << "\n\"" << inputBuffer << "\"" << " is spelled correctly.\n" << '\n';
 
-		// print suggested words based on edit distance
 		else {
 			cout << "\nDid you mean: " << '\n';
 			for (int i = 0; i < dictionary->mapCapacity(); i++) {
 				HashLink<string, int> *seeker = dictionary->mapTableLink(i);
 				while (seeker) {
 					string seekerKey = seeker->getKey();
-					/* result filters:
-                     * the length of the suggestion is at least the length of the misspelled word
-                     * the first letter of the misspelled word is correct
-                     * levenshtein distance between words is 1 or 2
-                     */
 					if (seekerKey.length() >= inputBuffer.length() && seekerKey[0] == inputBuffer[0]) {
-						// calculate edit distance between mispelled word and filtered words
 						int LD = calcLD(inputBuffer, seeker->getKey());
                     	if ((LD == 1 || LD == 2)) 
                     		cout << seekerKey << '\n';
